cfbf6aa4daab31f970524cfaaa4f4708bc5dc39e
YaelOmer <omeryael7@gmail.com>
2025-04-07 20:54:14 +0300

    שינוי אחד ברמות קושי

--- app/src/main/java/com/omer/yaellastfinal/Difficulty.java
+++ app/src/main/java/com/omer/yaellastfinal/Difficulty.java
@@ -3,10 +3,9 @@ package com.omer.yaellastfinal;
 public enum Difficulty {
     SUPEREASY(4, 2, 5),
     EASY(6, 4, 10),
-    EASYMID(8, 6, 15),
-    MEDIUM(10, 8, 20),
-    HARD(12, 10, 25),
-    EXTREME(14, 12, 30);
+    MEDIUM(8, 6, 15),
+    HARD(10, 8, 20),
+    EXTREME(12, 10, 25);
 
     private final int numOfJars;
     private final int numberOfColors;
